dbfb4e56e4df01d1a15302288a9b9bdf83d05c88
ksifnro4 <ksifnro4@gmail.com>
2025-02-19 23:50:13 +0900

    3度目のコミット

---
+++ src/main/java/jp/co/internous/ecsite/model/dto/HistoryDto.java
@@ -0,0 +1,31 @@
+package jp.co.internous.ecsite.model.dto;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
+import jp.co.internous.ecsite.model.domain.TblPurchase;
+
+public class HistoryDto {
+	private int id;
+	private int userId;
+	private int goodsId;
+	private String goodsName;
+	private int itemCount;
+	private int total;
+	private String createdAt;
+
+	public HistoryDto() {
+	}
+
+	public HistoryDto(TblPurchase entity) {
+		this.id = entity.getId();
+		this.userId = entity.getUserId();
+		this.goodsId = entity.getGoodsId();
+		this.goodsName = entity.getGoodsName();
+		this.itemCount = entity.getItemCount();
+		this.total = entity.getTotal();
+		Timestamp d = entity.getCreatedAt();
+		SimpleDateFormat f = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		this.createdAt = f.format(d);
+	}
+}
